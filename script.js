fa433b30a28b2aa69ca8e9a8e8e890ad42c91c17
Cakren574 <sabriganten321@gmail.com>
2025-06-01 16:47:20 +0700

    Update script.js
    Signed-off-by: Cakren574 <sabriganten321@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Ambil elemen-elemen yang dibutuhkan
 const popup = document.getElementById("popup");
 const popupImg = document.getElementById("popup-img");
 const galleryImgs = document.querySelectorAll(".gallery img");
 
-// Saat gambar diklik, munculkan pop-up
 galleryImgs.forEach(img => {
   img.addEventListener("click", () => {
     popup.style.display = "flex";
@@ -11,7 +9,7 @@ galleryImgs.forEach(img => {
   });
 });
 
-// Saat pop-up diklik, tutup pop-up
+
 popup.addEventListener("click", () => {
   popup.style.display = "none";
 });
